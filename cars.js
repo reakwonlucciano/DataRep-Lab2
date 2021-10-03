76b33023f3773728edc2d15348cee09985d86a5a
reakwonlucciano <62098320+reakwonlucciano@users.noreply.github.com>
2021-10-03 22:39:52 +0100

    Update cars.js

--- cars.js
+++ cars.js
@@ -1,12 +1,17 @@
-//this class is to 
+//this class is a blueprint to create objects
+
 class vehicle
 {
+    
+ //this constructor initializes data which takes three arguments.
+    
     constructor(make, model, year){
     this.make = make;
     this.model = model;
     this.year = year;
     }
-
+//this is a method (information) is used 
+    
     Information(){
         console.log(`Make:${this.make}`);
         console.log(`Model:${this.model}`);
@@ -27,6 +32,8 @@ Information(){
     console.log(`Doors: ${this.doors}`)
 }
 }
+//created a variable 
+
 let mycar = new Cars('VW', 'polo', 2021, 24);
 mycar.Information();
 // let myvehicle = new vehicle('VW', 'golf', 2021);
