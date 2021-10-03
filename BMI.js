6ad9235e7eed98d6fdc0b2e833d6edaa5fcfb6b7
reakwonlucciano <62098320+reakwonlucciano@users.noreply.github.com>
2021-10-03 22:34:23 +0100

    Update BMI.js

--- BMI.js
+++ BMI.js
@@ -1,16 +1,21 @@
-
+//the class is to create a object
 class BMI
 {
+    
+//the constructor initalizes data in the class. 
     constructor(height, weight){
         this.height =height;
         this.weight = weight;
     }
-   
+    
+//this is the method used to calculate.
     calculateBMI(){
         let bmi = this.weight/(this.height**2);
         return bmi;
     }
 }
+
+//
 
 let myBMI = new BMI(2, 90);
 let calc = myBMI.calculateBMI();
